3069838e6b7472b900b809f8631660dbce5ee46b
xx-Flex-xx <endlessxxxpain@gmail.com>
2023-02-13 17:02:24 +0300

    Git ignore

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,7 @@ const blogs_routes_1 = require("./routes/blogs.routes");
 const posts_routes_1 = require("./routes/posts.routes");
 const testing_routes_1 = require("./routes/testing.routes");
 const app = (0, express_1.default)();
-const port = 3333;
+const port = process.env.PORT || 3333;
 const parserMiddleware = (0, body_parser_1.default)({});
 app.use(parserMiddleware);
 app.use("/blogs", blogs_routes_1.h2BlogsRouter);
