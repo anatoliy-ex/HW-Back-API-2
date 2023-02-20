ffeadeebac05604d832cb1b03c0e56889af0ae29
xx-Flex-xx <endlessxxxpain@gmail.com>
2023-02-20 17:49:23 +0300

    New dist

--- dist/dataBase/blogs.db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.blogsDB = void 0;
-exports.blogsDB = [{
-        "id": "string",
-        "name": "string",
-        "description": "string",
-        "websiteUrl": "string"
-    }];
