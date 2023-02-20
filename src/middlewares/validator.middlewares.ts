f7965875283c439a2d75a2936342c1a4288e75dc
xx-Flex-xx <endlessxxxpain@gmail.com>
2023-02-20 18:31:57 +0300

    New wall

--- src/middlewares/validator.middlewares.ts
+++ src/middlewares/validator.middlewares.ts
@@ -6,41 +6,40 @@ import { body, validationResult } from 'express-validator';
 import{ blogsRepositoryDb} from "../repositories/blogs.repository.db";
 import {postsRepositoryDb} from "../repositories/posts.repository.db";
 
-export const findByIdBlogs : CustomValidator = value => {
-    let blog = blogsRepositoryDb.getBlogByID(value)
-    if (!blog){
-        throw new Error('Invalid blogId')
+export const findByIdBlogs : CustomValidator = async value=> {
+    let blog = await blogsRepositoryDb.getBlogByID(value)
+    if (blog === null){
+        throw new Error('not blogId')
     }
-    return true
 };
 
-export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) =>
-{
+export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const error = validationResult(req)
-    if (!error.isEmpty())
-    {
+    if (!error.isEmpty()) {
         return res.status(400).send({
             errorsMessages: error.array({onlyFirstError: true}).map(e => {
                 return {
-                     message: e.msg,
-                     field: e.param
+                    message: e.msg,
+                    field: e.param
                 }
             })
         })
     }
     next()
 }
+
 export const blogValidationMiddleware =
     [
-    body('name').trim().isLength({min: 1, max: 15}).isString().withMessage('name incorrect'),
+    body('name').trim().isLength({min: 1, max: 15}).isString().isString(),
     body('description').trim().isLength({min: 1, max: 500}).isString(),
     body('websiteUrl').trim().isLength({min: 1, max: 100}).matches(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/).isString(),
 ];
+
 
 export const postValidationMiddleware =
     [
     body('title').trim().isLength({min:1, max: 30}).isString(),
     body('shortDescription').trim().isLength({min:1,max:100}).isString(),
     body('content').trim().isLength({min:1, max: 1000}).isString(),
-    body('blogId').trim().custom(findByIdBlogs).isString()
+    body('blogId').trim().custom(findByIdBlogs).isString(),
 ];
